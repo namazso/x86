48486b66d3da75236bbc133823e11207820c64c3
namazso <admin@namazso.eu>
2022-02-28 17:44:47 +0100

    Add license

--- parse.ps1
+++ parse.ps1
@@ -1,3 +1,19 @@
+#   Pretty x86 instruction reference site generator
+#   Copyright (C) 2022  namazso <admin@namazso.eu>
+#   
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#   
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 [void](mkdir -Force svg)
 
 & mutool convert -o "svg/%d.svg" -F svg "325383-sdm-vol-2abcd.pdf"
